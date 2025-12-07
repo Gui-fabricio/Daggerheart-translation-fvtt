85cdf58170a54c02295da1a2e3f7275e81fbad51
Guilherme Fabricio <guilhermefabricio.lira@gmail.com>
2025-12-07 18:22:35 -0300

    validação mestre digital

--- babele-register.js
+++ babele-register.js
@@ -1,24 +1,9 @@
-Hooks.on("init", () => {
+Hooks.once("init", () => {
   if (typeof Babele !== "undefined") {
     Babele.get().register({
-      module: "daggerheart-pt-br",
-      lang: "pt-BR",
-      collections: [
-        "daggerheart.adversaries",     // Adversários
-		"daggerheart.ancestries",      // Ancestralidades
-        "daggerheart.armors",     	   // Armaduras
-        "daggerheart.bestforms",       // Forma de Fera
-        "daggerheart.classes",         // Classes  
-        "daggerheart.communities",     // Comunidades  
-        "daggerheart.consumables",     // Consumíveis  
-        "daggerheart.domains",         // Domínios  
-        "daggerheart.environments",    // Experiências
-        "daggerheart.journals",        // Journals
-        "daggerheart.loot",            // Tesouros
-        "daggerheart.subclasses",       // sub-classes
-	    "daggerheart.weapons",         // Armas  
-      ],
-      override: false  // Mantenha false para não sobrescrever originais
+      module: 'Daggerheart-translation-fvtt',
+      lang: 'pt-BR',
+      dir: 'compendium'
     });
   }
 });
